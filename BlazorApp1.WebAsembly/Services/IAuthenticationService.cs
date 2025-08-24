ff0cf218b765f41698d179f8818d1e1bf79521b0
Balázs <peter.balazs0921@gmail.com>
2025-08-24 17:13:21 +0200

    index on WebAssembly: 04d21b4 Merge pull request #28 from PBalazs0921/Authentication

---
+++ BlazorApp1.WebAsembly/Services/IAuthenticationService.cs
@@ -0,0 +1,11 @@
+using BlazorApp1.Entities.Dto;
+
+namespace BlazorApp1.WebAsembly.Services;
+
+public interface IAuthenticationService
+{
+    event Action<string?>? LoginChange;
+    ValueTask<string> GetJwtAsync();
+    Task LogoutAsync();
+    Task<DateTime> LoginAsync(UserLoginDto model);
+}
